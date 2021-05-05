400e35b722c141938170e0a1443f8b4ef108115d
Isuru Fernando <isuruf@gmail.com>
2021-05-05 12:06:03 -0500

    Rename libraries on win to make DLL import library the default

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,3 +19,13 @@ if errorlevel 1 exit 1
 :: Install.
 nmake install
 if errorlevel 1 exit 1
+
+move %LIBRARY_LIB%\fltk.lib %LIBRARY_LIB%\fltk_static.lib
+move %LIBRARY_LIB%\fltk_forms.lib %LIBRARY_LIB%\fltk_forms_static.lib
+move %LIBRARY_LIB%\fltk_images.lib %LIBRARY_LIB%\fltk_images_static.lib
+move %LIBRARY_LIB%\fltk_gl.lib %LIBRARY_LIB%\fltk_gl_static.lib
+
+move %LIBRARY_LIB%\fltk_SHARED.lib %LIBRARY_LIB%\fltk.lib
+move %LIBRARY_LIB%\fltk_forms_SHARED.lib %LIBRARY_LIB%\fltk_forms.lib
+move %LIBRARY_LIB%\fltk_images_SHARED.lib %LIBRARY_LIB%\fltk_images.lib
+move %LIBRARY_LIB%\fltk_gl_SHARED.lib %LIBRARY_LIB%\fltk_gl.lib
