b6bb3e0a8e672d10e6de761b1e7ac2f692453e88
Matthias Diener <mdiener@illinois.edu>
2021-09-09 14:29:03 -0500

    attempt to fix win lib file names

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -25,7 +25,3 @@ move %LIBRARY_LIB%\fltk_forms.lib %LIBRARY_LIB%\fltk_forms_static.lib
 move %LIBRARY_LIB%\fltk_images.lib %LIBRARY_LIB%\fltk_images_static.lib
 move %LIBRARY_LIB%\fltk_gl.lib %LIBRARY_LIB%\fltk_gl_static.lib
 
-move %LIBRARY_LIB%\fltk_SHARED.lib %LIBRARY_LIB%\fltk.lib
-move %LIBRARY_LIB%\fltk_forms_SHARED.lib %LIBRARY_LIB%\fltk_forms.lib
-move %LIBRARY_LIB%\fltk_images_SHARED.lib %LIBRARY_LIB%\fltk_images.lib
-move %LIBRARY_LIB%\fltk_gl_SHARED.lib %LIBRARY_LIB%\fltk_gl.lib
